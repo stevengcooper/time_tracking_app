fa6d51754fad63b0ed00088ae156c96f19c7f217
Steven Cooper <stevengcooper76@gmail.com>
2016-03-25 11:49:22 -0400

    Projects index displays Hours Logged in red if over allowed_hours. hours_logged and maximum_hours? instance methods in Project model.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,8 +2,12 @@ class Project < ActiveRecord::Base
   has_many :time_entries, dependent: :destroy
   has_many :developers, through: :time_entries
 
-  def self.hours_logged
-    TimeEntry.where(project_id: @self.id).each {|time| time.allowed_hours += 0 }
+  def hours_logged
+    time_entries.sum(:duration)
+  end
+
+  def over_maximum?
+    hours_logged > allowed_hours
   end
 
 end
