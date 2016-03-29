1dba20365f2b1d4e21397c7ea1e9638cef648032
Steven Cooper <stevengcooper76@gmail.com>
2016-03-29 19:08:23 -0400

    Added sessions#new before action.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  before_action :authenticate, only: [:edit, :update, :delete]
+  before_action :authenticate, only: [:new, :edit, :update, :delete]
 
   def new
     @developer = Developer.new
