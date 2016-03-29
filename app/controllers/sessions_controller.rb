74f05db0609096177f9eccd0dabd3664e8809ac7
Steven Cooper <stevengcooper76@gmail.com>
2016-03-29 19:11:10 -0400

    Added an exception for authentication on session#new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
-  before_action :authenticate, only: [:new, :edit, :update, :delete]
+  before_action :authenticate, only: [:edit, :update, :delete]
+  before_action :authenticate, except: [:new, :create]
 
   def new
     @developer = Developer.new
