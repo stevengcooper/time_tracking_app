996cbfdbae1f67b576f12fcf7914902b0b552fb9
Steven Cooper <stevengcooper76@gmail.com>
2016-03-29 23:33:09 -0400

    Before action in developers controller sets developer on edit, update, and destroy.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
-  before_action :authenticate, only: [:edit, :update, :delete]
+  before_action :set_developer, only: [:edit, :update, :destroy]
   before_action :authenticate, except: [:new, :create]
-  
+
 
   def new
     @developer = Developer.new
