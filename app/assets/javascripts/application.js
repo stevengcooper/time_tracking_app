e39c6c6f55c19857268a0d3b862a9a587f4dcddb
Steven Cooper <stevengcooper76@gmail.com>
2016-04-28 23:20:20 -0400

    Landing page is responsive and styled. Functional except for links.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-
+//= require modernizr
 //= require_tree .
 $(document).ready(function() {
   var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
