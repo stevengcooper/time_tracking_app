43cae59293b25ed2285a43d0869200e2bae7ba06
Steven Cooper <stevengcooper76@gmail.com>
2016-04-29 10:07:25 -0400

    All pages styled.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,19 +14,6 @@
 //= require jquery_ujs
 //= require modernizr
 //= require_tree .
-$(document).ready(function() {
-  var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
-  $('#js-centered-navigation-menu').removeClass("show");
-
-  menuToggle.on('click', function(e) {
-    e.preventDefault();
-    $('#js-centered-navigation-menu').slideToggle(function(){
-      if($('#js-centered-navigation-menu').is(':hidden')) {
-        $('#js-centered-navigation-menu').removeAttr('style');
-      }
-    });
-  });
-});
 
 $(document).ready(function($){
 	var introSection = $('#cd-intro-background'),
